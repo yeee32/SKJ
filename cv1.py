73dc4910cda157034f6d4d6d296583a58829949c
Vojtěch Ožana <97116766+yeee32@users.noreply.github.com>
2026-02-17 19:15:18 +0100

    prvni cviko

---
+++ cv1.py
@@ -0,0 +1,83 @@
+"""
+Script for the first laboratory.
+Add code by following instructions of the teacher.
+"""
+
+from typing import Any, List
+
+
+def add(a: Any, b: Any) -> Any:
+    """Adds parameters."""
+    return a + b
+
+def what_number(number: int|float) -> str:
+    """Returns string positive/zero/negative specifying
+    value of the number."""
+    if number == 0:
+        return "zero"
+    elif number > 0:
+        return "positive"
+    else:
+        return "negative"
+        
+
+def sum_of_numbers(lst) -> int:
+    """Returns sum of the numbers in the list."""
+    res = 0
+    for num in lst:
+        res += num
+
+    return res
+
+def ship_name(fleet: dict, designated_no: Any) -> Any:
+    """Return ship's name for specified designated number
+    from the fleet."""
+    # has dictionary a key? Use syntax: key in dictionary
+    return fleet[designated_no]
+
+def how_many_5(numbers: List[int|float]) -> int:
+    """Returns number of numbers greater than 5."""
+    # Modify example to take argument that specifies threshold
+    res = 0
+    for num in numbers:
+        if num == 5:
+            res += 1
+    return res
+
+def gen_list_gt(lst: List[int|float], no: int|float) -> List[int|float]:
+    """Returns list with numbers greater than no."""
+    #syntax: [ item for item in lst if_condition ]
+    my_list = []
+    for n in lst:
+        if n > no:
+            my_list.append(n)
+    return my_list
+
+    # return [num for num in lst if num > no]
+
+
+print(add(1, 3))
+# print(add([1, 2, 3], [4, 5, 6]))
+# Try addition of strings or different data type and see what happens
+
+#if statement example
+n = 5
+print("Number", n, "is:", what_number(n))
+
+#for example: sum of the list example
+lst = [1, 2, 3, 6, 7, 8]
+print("Sum is:", sum_of_numbers(lst))
+
+#dictionary example
+fleet = {'BS62': 'Pegasus', "BS75": "Galactica", 36: 'Valkirie'}
+designated_no = "BS62"
+print("We've got {} in the fleet".format(ship_name(fleet, designated_no)))
+
+#function to count how many numbers > 5 are in the list
+lst = [1, 2, 5, 6, 7, 10, 12, 40, 3]
+print("There are {} numbers greater than 5".format(how_many_5(lst)))
+
+#generating list example
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+no = 5
+print("List with numbers > {}: {}".format(no, gen_list_gt(lst, no)))
